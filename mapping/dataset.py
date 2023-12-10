6ab2aeefc72b6fe1a4e55b80234b6fe56a4b1926
linhsinkai <linhsinkai@learner.csie.ntu.edu.tw>
2023-12-10 20:43:35 +0800

    support domain clustered dataset

--- mapping/dataset.py
+++ mapping/dataset.py
@@ -9,6 +9,8 @@ class PseudoDataset(Dataset):
         n_data: int = 10000,
         domain_1: Tuple[float, float, int] = (0.0, 1.0, 1024),
         domain_2: Tuple[float, float, int] = (1.0, 2.0, 1024),
+        num_cluster: int = 10,
+        use_cluster: bool = True,
     ) -> None:
         """
         param:
@@ -19,12 +21,36 @@ class PseudoDataset(Dataset):
         super().__init__()
 
         self.n_data = n_data
+        self.num_cluster = num_cluster
+        self.use_cluster = use_cluster
 
-        mean_1, std_1, dim_1 = domain_1
-        mean_2, std_2, dim_2 = domain_2
+        self.data_dom_1 = torch.zeros((n_data, domain_1[2]))
+        self.data_dom_2 = torch.zeros((n_data, domain_2[2]))
 
-        self.data_dom_1 = torch.normal(mean=mean_1, std=std_1, size=(n_data, dim_1))
-        self.data_dom_2 = torch.normal(mean=mean_2, std=std_2, size=(n_data, dim_2))
+        if self.use_cluster:
+            for multiplier in range(self.num_cluster):
+                start = multiplier * (n_data // self.num_cluster)
+                end = (multiplier + 1) * (n_data // self.num_cluster)
+                size = end - start
+
+                mean_1, std_1, dim_1 = domain_1
+                mean_2, std_2, dim_2 = domain_2
+
+                mean_1 += multiplier * 0.5
+                mean_2 += multiplier * 0.5
+
+                std_1 += multiplier * 0.1
+                std_2 += multiplier * 0.1
+
+                self.data_dom_1[start:end] = torch.normal(mean=mean_1, std=std_1, size=(size, dim_1))
+                self.data_dom_2[start:end] = torch.normal(mean=mean_2, std=std_2, size=(size, dim_2))
+
+        else:
+            mean_1, std_1, dim_1 = domain_1
+            mean_2, std_2, dim_2 = domain_2
+
+            self.data_dom_1 = torch.normal(mean=mean_1, std=std_1, size=(n_data, dim_1))
+            self.data_dom_2 = torch.normal(mean=mean_2, std=std_2, size=(n_data, dim_2))
 
     def __len__(self) -> int:
         return self.n_data
